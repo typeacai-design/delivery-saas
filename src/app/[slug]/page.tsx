7c51c7afac7747ebac6696e2cedec8c3ede45f6a
Rick <ranieryrick4@gmail.com>
2026-08-24 19:02:32 -0300

    feat: rota dinamica /[slug] para cardapio na raiz
    Agora funciona: /typeacai ao inves de /cardapio/typeacai

---
+++ src/app/[slug]/page.tsx
@@ -0,0 +1,6 @@
+// Rota dinâmica na raiz: /[slug] mostra o cardápio público
+// Ex: /typeacai → cardápio da loja typeacai
+
+import CardapioPublicoPage from '../cardapio/[slug]/page'
+
+export default CardapioPublicoPage
