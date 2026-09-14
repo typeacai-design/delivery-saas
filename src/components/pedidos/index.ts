cd16e35fd38f73f35db24d50314687c8bc19cb6d
Rick <typeacai@gmail.com>
2026-09-14 11:13:58 -0300

    chore: remove componentes quebrados (serão refeitos corretamente)

--- src/components/pedidos/index.ts
+++
@@ -1,9 +0,0 @@
-// Componentes de Pedidos - Exportação centralizada
-export { PedidoCard } from './PedidoCard'
-export { PedidoStats, PedidoFiltros } from './PedidoFiltros'
-export {
-  PedidoDetalhesModal,
-  PedidoDescontoModal,
-  PedidoCancelarModal,
-  PedidoEditarModal,
-} from './PedidoModals'
